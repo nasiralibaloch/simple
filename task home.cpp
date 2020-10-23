3a35cefe7cee7d489d7fc8f9da82029213d8d290
nasiralibaloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-10-23 22:05:10 +0500

    Add files via upload

---
+++ task home.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the numbers"<<endl;
+	cin>>n;
+	if(n>80&&n<90)
+	{
+		cout<<"grade A"<<endl;
+	}
+	else if(n>70&&n<80)
+	{
+		
+		cout<<"grade B"<<endl;
+	}
+	else if(n>60&&n<70)
+	{
+	cout<<"grade c"<<endl;	
+	}
+	else 
+	{
+	cout<<"grade f"<<endl;	
+	}
+	
+	return 0;
+}
